7fba25a47ab8e528739c3e570c4b39389fc062bd
Anna Mikhaylova <annamikhailova1406@yandex.ru>
2026-04-04 10:56:26 -0700

    second task

---
+++ app/src/main/java/com/example/Calculator.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static int getPoshlina(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Привет, введите цену");
+
+        int price = sc.nextInt();
+        System.out.println("Введите вес товара");
+        int weight = sc.nextInt();
+
+        return price/100 + weight*100;
+    }
+}
